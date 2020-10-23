881526242ec085b342c8cad7a909fafba4f2cb66
Eliott <eliott.jarmoune@outlook.com>
2020-10-23 11:43:41 +0200

    done ask to eat

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "!";
+// const prefix = "!";
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
+db.defaults({ restaurant: [], count: 0 }).write();
+db.set('user.name', 'typicode').write();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,35 +18,32 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.author.bot) return;
     // if (!msg.content.startsWith(prefix)) return;
-
     // const commandBody = msg.content.slice(prefix.length);
     // const args = commandBody.split(' ');
     // const command = args.shift().toLowerCase();
+
+    const getId = db.get('restaurant').value();
+    const item = getId[Math.floor(Math.random() * getId.length)];
+    const getRestau = item.title;
+    console.log(getRestau);
+
+    // A garder pour ajoutez le restaurant sur la BD
+    
+    // const getLastId = db.get('restaurant').takeRight(1).value();
+
+    // db.get('restaurant')
+    //   .push({ id: getLastId + 1, title: 'Mcdo'})
+    //   .write();
 
     if (msg.content === "ping") {
         msg.reply(`pas pong`);
     }
 
     const str = msg.content.replace(/\s/g, '');
-    console.log(str);
     if (str == "j'aifaim") {
-        msg.reply(`Bas mange frr!`);
+        msg.reply(`, tu peux manger chez ${getRestau}`);
     }
 });
-
-// client.on('message', msg => {
-//     if (msg.author.bot) return;
-//     if (!msg.content.startsWith(prefix)) return;
-
-//     const commandBody = msg.content.slice(prefix.length);
-//     const args = commandBody.split(' ');
-//     const command = args.shift().toLowerCase();
-
-//     if (command === "faim") {
-//         msg.reply(`Bas mange frr!`);
-//     }
-// });
-
 
 
 client.login(process.env.TOKEN);
