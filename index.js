35aa787aca24a6d72407e093398d5049d7367010
Eliott <eliott.jarmoune@outlook.com>
2020-10-25 17:18:28 +0100

    add issues of help, add new features in dbhelp file

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const client = new Discord.Client();
 // const prefix = "!";
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
+const adapterhelp = new FileSync('dbhelp.json');
+const dbhelp = low(adapterhelp);
+
+
 db.defaults({ restaurant: [], count: 0 }).write();
+dbhelp.defaults({ help: []}).write();
+
 db.set('user.name', 'typicode').write();
 
 client.on('ready', () => {
@@ -18,12 +25,15 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.author.bot) return;
 
+    const str = msg.content.replace(/\s/g, '');
+
     // if (!msg.content.startsWith(prefix)) return;
     // const commandBody = msg.content.slice(prefix.length);
     // const args = commandBody.split(' ');
     // const command = args.shift().toLowerCase();
 
-    const getId = db.get('restaurant').value();
+    const getIdRestau = db.get('restaurant').value();
+    const getIdHelp = dbhelp.get('help').value();
     // A garder pour ajoutez le restaurant sur la BD
 
     // const getLastId = db.get('restaurant').takeRight(1).value();
@@ -35,10 +45,10 @@ client.on('message', msg => {
         msg.reply(`pas pong`);
     }
 
-    const str = msg.content.replace(/\s/g, '');
+
     if (str == "j'aifaim") {
-        if (getId.length > 0) {
-            const item = getId[Math.floor(Math.random() * getId.length)];
+        if (getIdRestau.length > 0) {
+            const item = getIdRestau[Math.floor(Math.random() * getIdRestau.length)];
             const getRestau = item.title;
 
             if (msg.channel.type === 'dm') {
@@ -50,6 +60,14 @@ client.on('message', msg => {
         } else {
             msg.reply(`Mmmmh, j'espère que tu n'as pas trop faim ${msg.author.username}`);
         }
+    }
+
+
+    if (str == "foodrhelp") {
+        getIdHelp.map((x) => {
+            msg.reply(`${x.features}`);
+        });
+
     }
 });
 
